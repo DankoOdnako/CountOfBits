2af7226c60be7a28d25570f736af871c7fb1a892
Даниил <@DESKTOP-NA8A3QK>
2023-02-11 12:41:38 +0700

    Добавьте файлы проекта.

---
+++ Kata3angle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace Kata3angle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBits());
+        }
+
+        public static int CountBits(int n)
+        {
+            var binaryNumber = string.Empty;
+            int count = 0;
+
+            while (n > 0)
+            {
+                binaryNumber = (n % 2) + binaryNumber;
+                n /= 2;
+            }
+
+            char[] binaryChars = binaryNumber.ToCharArray();
+            for(int i = 0; i < binaryChars.Length; i++)
+            {
+                if (binaryChars[i] == '1')
+                    count++;
+            }
+            return count;
+        }
+    }
+}
